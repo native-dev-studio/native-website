4e28dbc7a47576c7f9aab14a53d5cdd1226ed47d
Kel Kanhirun <kel@native.dev>
2022-09-05 17:38:58 -0400

    feat: adds gray color var

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray': '#1B1B1B'
+      }
+    },
   },
   plugins: [],
 };
