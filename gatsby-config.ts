7e97ab1866741f52337766147e4767ca3d07495e
Kel Kanhirun <kel@native.dev>
2022-09-06 12:30:09 -0400

    feat: adds tab logo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,7 +16,14 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        name: "Native",
+        short_name: "Native",
+        start_url: "/",
+        icon: "src/images/native-logo.svg",
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     "gatsby-plugin-mdx",
