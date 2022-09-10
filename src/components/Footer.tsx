b72014d3ff7ae8bf7a597c580f8675e754d8f78b
Kel Kanhirun <kel@native.dev>
2022-09-10 12:18:03 -0400

    fix: footer styling, reuse

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,86 +18,64 @@ const Footer = () => {
             mb-5
             '
           />
-          <div className='
-            lg:grid-container
-            md:grid md:grid-cols-8 md:mx-4 md:gap-y-5
-            grid grid-cols-4 mx-4 gap-y-5
-            '>
-            <p className='
-              lg:my-10 lg:col-start-2 lg:col-span-10
-              md:col-span-8
-              col-span-4 my-2
-              '>
+          <div className='grid-container'>
+            <p className='mt-2 mb-5'>
               Interested in learning more about our approach to custom software? Reach out to&nbsp;
               <a href='mailto:workwith@native.dev' className='underline'>
                 workwith@native.dev
               </a>.
             </p>
-            <ul className='
-              lg:col-start-2
-              col-span-2
-              '>
-              <li className='font-bold lg:text-md text-sm'>
-                Contact
-              </li>
-              <li>
-                <a href='mailto:workwith@native.dev' className='text-white lg:text-sm text-xs'>
-                  workwith@native.dev
-                </a>
-              </li>
-              <li>
-                <a href='mailto:careers@native.dev' className='text-white lg:text-sm text-xs'>
-                  careers@native.dev
-                </a>
-              </li>
-              <li>
-                <a href='mailto:press@native.dev' className='text-white lg:text-sm text-xs'>
-                  press@native.dev
-                </a>
-              </li>
-            </ul>
-            <ul className='
-              lg:col-start-4 lg:col-span-2
-              md:col-start-4
-              col-span-4
-              '>
-              <li className='font-bold lg:text-md text-sm'>
-                Learn
-              </li>
-              <li>
-                <a href='https://pitch.com/v/website-u6wxwg' className='text-white lg:text-sm text-xs'>
-                  Native Pitch Deck
-                </a>
-                <span className='ml-2 rounded-full bg-red py-1.5 px-4 text-xs bg-blue'>
-                  New!
-                </span>
-              </li>
-            </ul>
-            <ul className='
-              lg:col-start-6
-              md:col-start-7 md:col-span-2
-              col-span-4
-              '>
-              <li className='font-bold lg:text-md text-sm'>
-                Connect
-              </li>
-              <li>
-                <a href='https://www.linkedin.com/company/native-dev' className='text-white lg:text-sm text-xs'>
-                  LinkedIn
-                </a>
-              </li>
-              <li>
-                <a href='https://github.com/native-dev-studio' className='text-white lg:text-sm text-xs'>
-                  Github
-                </a>
-              </li>
-            </ul>
-            <div className='
-              divider
-              lg:col-start-2 lg:col-span-10
-              md:col-span-8
-              col-span-4 -mb-2
-            '/>
+            <div className='flex md:flex-row flex-col md:space-y-0 space-y-5'>
+              <ul className='md:flex-auto'>
+                <li className='font-bold md:text-md text-sm'>
+                  Contact
+                </li>
+                <li>
+                  <a href='mailto:workwith@native.dev' className='text-white md:text-sm text-xs'>
+                    workwith@native.dev
+                  </a>
+                </li>
+                <li>
+                  <a href='mailto:careers@native.dev' className='text-white md:text-sm text-xs'>
+                    careers@native.dev
+                  </a>
+                </li>
+                <li>
+                  <a href='mailto:press@native.dev' className='text-white md:text-sm text-xs'>
+                    press@native.dev
+                  </a>
+                </li>
+              </ul>
+              <ul className='md:flex-auto'>
+                <li className='font-bold md:text-md text-sm'>
+                  Work with us
+                </li>
+                <li>
+                  <a href='https://pitch.com/v/website-u6wxwg' className='text-white md:text-sm text-xs'>
+                    Native Pitch Deck
+                  </a>
+                  <span className='ml-2 rounded-full bg-red py-1.5 px-4 text-xs bg-blue'>
+                    New!
+                  </span>
+                </li>
+              </ul>
+              <ul className='md:flex-auto'>
+                <li className='font-bold md:text-md text-sm'>
+                  Connect
+                </li>
+                <li>
+                  <a href='https://www.linkedin.com/company/native-dev' className='text-white md:text-sm text-xs'>
+                    LinkedIn
+                  </a>
+                </li>
+                <li>
+                  <a href='https://github.com/native-dev-studio' className='text-white md:text-sm text-xs'>
+                    Github
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='divider' />
             <p className='
               lg:col-start-2 lg:col-span-10
               col-span-4 text-xs
