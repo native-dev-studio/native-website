0ab81dfdcad2916307fd28731d1fd3913d5fc08b
Kel Kanhirun <kel@native.dev>
2022-09-13 00:41:27 -0400

    feat: content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const IndexPage = () => {
               Expert partners in Web3 product, engineering & strategy
             </h1>
             <p>
-              Native is not just any studio that helps companies build products on Web3. Discover what makes us an effective partner for companies like a16z and ConsenSys on their next big crypto project.
+              Native is not just any studio that helps companies build products on Web3. Discover what makes us an effective partner for industry leaders like a16z and ConsenSys on their next big crypto project.
             </p>
           </section>
           <section className='
