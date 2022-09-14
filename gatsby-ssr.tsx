ea6200dc52d15e8f6023e767eb7ad1b58ebac568
Kel Kanhirun <kel@native.dev>
2022-09-14 15:46:26 -0400

    feat: adds meta tags for opengraph

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -5,9 +5,14 @@ export const onPreRenderHTML: GatsbySSR["onPreRenderHTML"] = ({ getHeadComponent
   const headComponents = getHeadComponents();
   const key = headComponents.length;
 
+  const description = "We're an independent studio helping startups & businesses build their mission-critical products on Web3.";
+
   replaceHeadComponents([
     ...headComponents,
     <title key={key}>Native</title>,
     <meta name='icon' href='./src/images/icon.png' />,
+    <meta property="og:title" content="Native" />,
+    <meta property="og:type" content="website" />,
+    <meta property="og:description" content={description} />,
   ])
 }
